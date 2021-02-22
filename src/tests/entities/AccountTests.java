99f02e965e4fb9a76290e4912ba22f01af04ee80
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-02-22 11:25:10 -0300

    withdraw tests

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -44,4 +44,23 @@ public class AccountTests {
 		Assertions.assertTrue(expectedValue == acc.getBalance());
 		Assertions.assertTrue(result == initialBalance);
 	}
+	
+	@Test
+	public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+		
+		Account acc = AccountFactory.createAccount(800.0);
+		
+		acc.withdraw(500.0);
+		
+		Assertions.assertEquals(300.0, acc.getBalance());
+	}
+	
+	@Test
+	public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> { 
+			Account acc =  AccountFactory.createAccount(800.0);
+			acc.withdraw(801.0);
+		});
+	}
 }
